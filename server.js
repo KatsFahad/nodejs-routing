addf7f3b0bb46712efa6d5725825bca9ed60fdd2
katemba fahad <katsfahad008@gmail.com>
2024-07-30 17:04:38 +0300

    Added init, packages

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http')
 const fs = require('fs')
+const _ = require('lodash')
 
 const server = http.createServer((req, res)=>{
-    console.log(req.url, req.method)
+
+    const num = _.random(0, 20);
+
+    console.log(num)
+
+    const hello = _.once(()=>{
+        console.log('hello')
+    })
+    hello()
+    hello()
 
     res.setHeader('Content-Type', 'text/html')
 
@@ -11,12 +21,20 @@ const server = http.createServer((req, res)=>{
     switch(req.url){
         case '/':
             path += 'index.html'
+            res.statusCode = 200
             break;
         case '/about':
             path += 'about.html'
+            res.statusCode = 200
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('location', '/about');
+            res.end()
             break;
         default:
             path += '404.html'
+            res.statusCode = 400
             break;
     }
 
